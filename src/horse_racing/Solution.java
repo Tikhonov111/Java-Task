cb19c32c9e230b0111367b1b45dd74b98b0c8557
Tikhonov111 <blondie138@mail.ru>
2022-05-25 09:09:13 +0300

    multithreading on horse racing

--- src/horse_racing/Solution.java
+++ src/horse_racing/Solution.java
@@ -12,6 +12,70 @@ import java.util.List;
 //        2.2. Подождать, пока она завершит гонку. Подумай, какой метод нужно использовать для этого.
 //        2.3. Не считать такую лошадь финишировавшей.
 
+public class Solution {
 
+    public static void main(String[] args) throws InterruptedException {
+        List<Horse> horses = prepareHorsesAndStart(10);
+        while (calculateHorsesFinished(horses) != horses.size()) {
+        }
+    }
+
+    public static int calculateHorsesFinished(List<Horse> horses) throws InterruptedException {
+
+        int finishedCount = 0;
+        //напишите тут ваш код
+        for (Horse horse : horses) {
+            if  (horse.isFinished()) {
+                finishedCount++;
+            }
+
+            if (!(horse.isFinished())) {
+                System.out.println("Waiting for " + horse.getName());
+                horse.join();
+            }
+        }
+        return finishedCount;
+    }
+
+    public static List<Horse> prepareHorsesAndStart(int horseCount) {
+        List<Horse> horses = new ArrayList<>(horseCount);
+        String number;
+        for (int i = 1; i < horseCount + 1; i++) {
+            number = i < 10 ? ("0" + i) : "" + i;
+            horses.add(new Horse("Horse_" + number));
+        }
+
+        System.out.println("All horses start the race!");
+        for (int i = 0; i < horseCount; i++) {
+            horses.get(i).start();
+        }
+        return horses;
+    }
+}
+
+class Horse extends Thread {
+
+    private boolean isFinished;
+
+    public Horse(String name) {
+        super(name);
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public void run() {
+        String s = "";
+        for (int i = 0; i < 1001; i++) {   // Delay
+            s += "" + i;
+            if (i == 1000) {
+                s = " has finished the race!";
+                System.out.println(getName() + s);
+                isFinished = true;
+            }
+        }
+    }
+}
 
 
