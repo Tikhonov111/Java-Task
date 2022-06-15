18afd2348b9df7dc3673857d48f4c52b233bc0d2
Tikhonov111 <blondie138@mail.ru>
2022-06-15 10:23:00 +0300

    reverse file

---
+++ src/reverse_file/Solution.java
@@ -0,0 +1,29 @@
+package reverse_file;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputFile = reader.readLine();
+        String outputFile = reader.readLine();
+
+        try (FileInputStream fileInputStream = new FileInputStream(inputFile);
+             FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+
+            List<Integer> inputBytes = new ArrayList<>();
+            while (fileInputStream.available() > 0) {
+                inputBytes.add(fileInputStream.read());
+            }
+
+            Collections.reverse(inputBytes);
+            for (Integer aByte : inputBytes) {
+                fileOutputStream.write(aByte);
+            }
+        }
+    }
+}
+
